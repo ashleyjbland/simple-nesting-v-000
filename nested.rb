0befb787ef0dae2028e7b1d2ea3b4094200a0ae8
Ashley Bland <ashleyjbland@gmail.com>
2018-09-07 22:33:58 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -104,7 +104,7 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     programmer_hash[:alan_kay][:known_for] = alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = "GUI"
 
 
 end
@@ -125,5 +125,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+		 programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
 
 end
